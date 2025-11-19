d969b9043a8124fa74972f1857b6c2675d2a09e7
karrola <karolinahadzinska@gmail.com>
2025-11-19 23:04:28 +0100

    models

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Reservation, ParkingSpace
+
 # Register your models here.
+
+
+# rejestracja Reservation w django admin
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("parking_space", "number_plate", "date_start", "time_start", "date_end", "time_end", "user")
+    search_fields = ("parking_space", "number_plate", "date_start", "time_start", "date_end", "time_end", "user")
+    ordering = ("parking_space",)
+
+admin.site.register(Reservation, ReservationAdmin)
+
+# rejestracja ParkingSpace w django admin
+class ParkingSpaceAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+admin.site.register(ParkingSpace, ParkingSpaceAdmin)
+
+
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+from django.conf import settings
+
 # Create your models here.
+
+# model miejsca parkingowego
+class ParkingSpace(models.Model):
+    name = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.name
+
+# model rezerwacji
+class Reservation(models.Model):
+    date_start = models.DateField()
+    date_end = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reservations')
+    parking_space = models.ForeignKey(ParkingSpace, on_delete=models.CASCADE, related_name='reservations')
+    number_plate = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f"Rezerwacja #{self.id}"
